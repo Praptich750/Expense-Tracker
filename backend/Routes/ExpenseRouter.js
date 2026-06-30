574c962d1e6962af8d47a1dced958e855c59b953
Bholuch <bholuchaudhary694@gmail.com>
2026-06-30 18:16:49 +0530

    update project

---
+++ backend/Routes/ExpenseRouter.js
@@ -0,0 +1,14 @@
+const { fetchExpenses, addExpenses, deleteExpenses } = require('../Controllers/ExpenseController.js');
+
+const router = require('express').Router();
+
+router.get('/', (req,res)=> res.send('expense get method working'));
+
+//fetch all the expenses of user based n user_id
+router.get('/', fetchExpenses);
+//add Expenses
+router.post('/', addExpenses);
+// delete Expenses
+router.delete('/', deleteExpenses);
+
+module.exports = router;
